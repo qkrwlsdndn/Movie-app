05d26aca757935fc593f3e561ae91b43a0859364
qkrwlsdndn <fhfhfk12@naver.com>
2024-12-05 17:09:22 +0900

    미니프로젝트

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import MovieCard from './components/MovieCard';
-import MovieDetail from './components/MovieDetail';
-import movieListData from './data/movielistdata.json';
+import movieListData from './data/movieListData.json';
 
 const App = () => {
+  const [movies] = useState(movieListData.results); 
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="movie-list">
-              {movieListData.results.map((movie) => (
-                <Link to={`/detail/${movie.id}`} key={movie.id}>
-                  <MovieCard
-                    poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    title={movie.title}
-                    rating={movie.vote_average}
-                  />
-                </Link>
-              ))}
-            </div>
-          }
+    <div className="movie-list">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+          title={movie.title}
+          rating={movie.vote_average}
         />
-        <Route path="/detail/:id" element={<MovieDetail />} />
-      </Routes>
-    </BrowserRouter>
+      ))}
+    </div>
   );
 };
 
